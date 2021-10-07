0ea9d32116964340d9ec3b27bd45ef72119f1f01
fiztech-code <66762914+fiztech-code@users.noreply.github.com>
2021-10-07 15:06:48 -0400

    updated init code

--- examples/BeanMPX_Example/BeanMPX_Example.ino
+++ examples/BeanMPX_Example/BeanMPX_Example.ino
@@ -7,19 +7,20 @@ uint32_t timer = 0;
 uint8_t fuel[] =      {0x62, 0xA4, 0x3C}; // Fuel D0 153-40 
 uint8_t engTemp[]  =  {0x62, 0x2C, 0xA5}; // Engine Temp D0 90-255 
 uint8_t gear[] =      {0x62, 0x40, 0x08, 0x10}; // P,R,N,D,M,3,2,L D0 11111111 (128, 64, 32, 16, 8, 4, 2, 1); Manual mode: 5,4,3,2,L D1 ---11111 (16, 8, 4, 2, 1)
-uint8_t ect[] =       {0x62, 0xD2, 0x08, 0x10}; // ECT PWR, SNOW, CRUISE D0 --11--1- (32,16,2); CRUISE-FLASH, ECT PWR-FLASH D1 -11----- (64,32);
+uint8_t ect[] =       {0x62, 0xD2, 0x08, 0x10, 0x00}; // ECT PWR, SNOW, CRUISE D0 --11--1- (32,16,2); CRUISE-FLASH, ECT PWR-FLASH D1 -11----- (64,32); Beep D2 ----1--- (8) toggle on/off 
 uint8_t seatBelt[] =  {0x62, 0xDF, 0x10, 0x80}; // DOOR D0 ---1---- (16);  SEAT BELT D1 1------- (128)
 
 // DOOR-FLASH
 uint8_t batt[] = {0x62, 0xD4, 0x28}; // DOOR-FLASH, BATT, OIL D0 -111---- (64, 32, 16) 
-uint8_t door[] = {0x62, 0xFA, 0xff}; // DOOR-FLASH D0 11111111 (255); *important mid:0xD4 D0:0x28 D0 -1------ must be set first
+uint8_t door[] = {0x62, 0xFA, 0xff}; // DOOR-FLASH D0 11111111 (255); *important mid:0xD4 D0:0x28 D0 -1------ must be set first //
 
 void setup() {
-  bean.ackMsg((const uint8_t[]) {0xFE}); // Messages to acknowledge
-  bean.begin();  
-  
   Serial.begin(115200);
+  while (!Serial) {;}  
   Serial.println("BeanMPX");
+  
+  bean.ackMsg((const uint8_t[]) {0xFE}, 1); // Acknowledge Messages, Length  
+  bean.begin();  
 }
 
 void loop() {  
